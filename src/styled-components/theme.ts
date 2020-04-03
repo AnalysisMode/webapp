a60b1e007d852114e922187a71979c960847b833
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-03 20:49:12 +0200

    Add the basic theme el according to design

--- src/styled-components/theme.ts
+++ src/styled-components/theme.ts
@@ -6,9 +6,31 @@ import { DefaultTheme } from 'styled-components'
 declare module 'styled-components' {
     export interface DefaultTheme {
         colors: {
-            main: string
-            lightGrey: string
-            white: string
+            primaryGreen: string
+            lightGreen: string
+            darkGreen: string
+            brown: string
+            red: {
+                [key: string]: string
+            }
+            blue: {
+                [key: string]: string
+            }
+            dark: {
+                [key: string]: string
+            }
+            light: {
+                [key: string]: string
+            }
+        }
+        font: {
+            family: {
+                title: string
+                body: string
+            }
+            size: {
+                [key: string]: string
+            }
         }
         spacing: {
             tiny: number
@@ -22,9 +44,39 @@ declare module 'styled-components' {
 // Export theme implementing interface
 export const theme: DefaultTheme = {
     colors: {
-        main: 'rgb(0, 136, 102)',
-        lightGrey: 'rgb(162, 162, 162)',
-        white: 'white',
+        primaryGreen: '#2BBD7E',
+        lightGreen: '#69F0AE',
+        darkGreen: '#219653',
+        brown: '#F2994A',
+        red: {
+            'shade-1': '#EB5757',
+        },
+        blue: {
+            'shade-1': '#2F80ED',
+            'shade-2': '#56CCF2',
+            'shade-3': '#BBE4DD',
+        },
+        dark: {
+            'shade-1': '#000000',
+            'shade-2': '#01170E',
+            'shade-3': '#06281A',
+        },
+        light: {
+            'shade-1': '#ffffff',
+            'shade-2': '#e5e5e5',
+        },
+    },
+    font: {
+        family: {
+            title: 'Brinnan',
+            body: 'Neutrif Pro',
+        },
+        size: {
+            smaller: '16px',
+            small: '18px',
+            medium: '26px',
+            large: '46px',
+        },
     },
     spacing: {
         large: 30,
