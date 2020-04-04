4eec6ddf3c769eee5ce4f3e6e2cc6f4575e37622
zoeyzou <13701719+zoeyzou@users.noreply.github.com>
2020-04-04 11:56:17 +0200

    Add basic button component

---
+++ src/components/button/button.tsx
@@ -0,0 +1,76 @@
+import React, { FC } from 'react'
+import styled, { css } from 'styled-components/macro'
+
+interface Props {
+    variant?: 'primary' | 'secondary' | 'link'
+    disabled?: boolean
+    onClick?: () => void
+}
+
+const StyledButton = styled.button<Props>`
+    font-size: ${({ theme }) => theme.font.size.smaller};
+    font-weight: bold;
+    font-family: ${({ theme }) => theme.font.family.title};
+    padding: 13px 23px;
+    text-transform: lowercase;
+    cursor: pointer;
+    transition: all 0.2s ease-out;
+    border: none;
+    background-color: transparent;
+
+    :hover {
+                transition: all 0.2s ease-out;
+                transform: scale(1.05);
+            }
+
+    ${({ disabled }) =>
+        disabled &&
+        css`
+            cursor: not-allowed;
+        `}
+
+    /* primary button style */
+    ${({ variant, theme }) =>
+        variant === 'primary' &&
+        css`
+            background-color: ${theme.colors.primaryGreen};
+            color: ${theme.colors.light['shade-1']};
+        `}
+    
+    /* secondary button style */
+    ${({ variant, theme }) =>
+        variant === 'secondary' &&
+        css`
+            color: ${theme.colors.primaryGreen};
+            border: 1px solid ${theme.colors.primaryGreen};
+        `}
+    
+    /* link style */
+    ${({ variant, theme }) =>
+        variant === 'link' &&
+        css`
+            color: ${theme.colors.primaryGreen};
+
+            :hover {
+                text-decoration: underline;
+            }
+        `}
+
+`
+
+export const Button: FC<Props> = ({
+    variant = 'primary',
+    disabled = false,
+    onClick,
+    children,
+}) => {
+    return (
+        <StyledButton
+            variant={variant}
+            onClick={onClick ? onClick : undefined}
+            disabled={disabled}
+        >
+            {children}
+        </StyledButton>
+    )
+}
