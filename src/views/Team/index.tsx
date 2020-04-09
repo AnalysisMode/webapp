1294974873504833702223b63c62335b9cd96532
tomekgora <tomekgora@gmail.com>
2020-04-09 14:23:17 +0200

    rudimetarily added advisors to Team view

--- src/views/Team/index.tsx
+++ src/views/Team/index.tsx
@@ -7,17 +7,36 @@ import ViewTitle from '../../components/ViewTitle'
 import TeamMemberCard from '../../components/TeamMemberCard'
 
 import team from '../../utils/team'
+import advisors from '../../utils/advisors'
 
 export default () => {
     return (
         <Team.Layout>
             <ViewTitle
-                title={'the team'}
+                title={'The Team'}
                 subtitle={'Those who seek your contribution'}
                 key={'team-view-title'}
             />
             <Team.TeamWrapper>
                 {team.map((member, i) => {
+                    return (
+                        <TeamMemberCard
+                            name={member.name}
+                            expertise={member.expertise}
+                            imagePath={member.imagePath}
+                            linkedinUrl={member.linkedinUrl}
+                            key={`member-${member.name}`}
+                        />
+                    )
+                })}
+            </Team.TeamWrapper>
+            <ViewTitle
+                title={'Supporters & Advisors'}
+                subtitle={'Those who are helping the project'}
+                key={'advisors-view-title'}
+            />
+            <Team.TeamWrapper>
+                {advisors.map((member, i) => {
                     return (
                         <TeamMemberCard
                             name={member.name}
@@ -44,7 +63,7 @@ const Team = {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
   `,
 }
