2f436d6a7d6ae5acdc946f80264fdb781eab1368
Andrey Shelkovnikov <shelkov09@gmail.com>
2020-04-09 19:46:11 +0300

    implement mobile navigation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,12 @@
 // libs
 import React from 'react'
 import Styled from 'styled-components'
+import { usePath } from 'hookrouter'
 
 // components
 import Brand from '../Brand'
 import Nav from '../Nav'
-import { usePath } from 'hookrouter'
+import MobileNav from '../MobileNav'
 
 export default () => {
     const isGameView = usePath().indexOf('/game') > -1
@@ -19,6 +20,9 @@ export default () => {
                 <Navbar.Nav>
                     <Nav isGameView={isGameView} />
                 </Navbar.Nav>
+                <Navbar.NavMobile>
+                    <MobileNav isGameView={isGameView} />
+                </Navbar.NavMobile>
             </Navbar.Layout>
         </Navbar.Wrapper>
     )
@@ -26,23 +30,34 @@ export default () => {
 
 const Navbar = {
     Wrapper: Styled.header`
-    display: flex;
-    height: 150px;
-    margin: 0 200px;
-    background: transparent;
-    align-items: center;
+      display: flex;
+      height: 150px;
+      margin: 0 200px;
+      background: transparent;
+      align-items: center;
     `,
     Layout: Styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 45px;
-    justify-content: space-between;
-    width: 100%;
-  `,
+      display: flex;
+      flex-direction: row;
+      height: 45px;
+      justify-content: space-between;
+      width: 100%;
+    `,
     Brand: Styled.div`
-    display: flex;
-  `,
+      display: flex;
+    `,
     Nav: Styled.div`
-    display: flex;
+      display: flex;
+      
+      @media (max-width: 1200px) {
+        display: none;
+      }
+    `,
+    NavMobile: Styled.div`
+      display: flex;
+      
+      @media (min-width: 1200px) {
+        display: none;
+      }
     `,
 }
